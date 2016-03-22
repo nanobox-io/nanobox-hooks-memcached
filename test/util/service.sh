90cfc63f04e1a99cc860aa4f4f01e2fa38a0846a
Braxton Huggins <braxton@pagodabox.com>
2016-03-22 16:59:15 -0600

    redo tests, rebuilt payloads using a script, modify the scripts

---
+++ test/util/service.sh
@@ -0,0 +1,74 @@
+
+service_name="Memcached"
+default_port=11211
+
+wait_for_running() {
+  container=$1
+  until docker exec ${container} bash -c "ps aux | grep [m]emcached"
+  do
+    sleep 1
+  done
+}
+
+wait_for_listening() {
+  container=$1
+  ip=$2
+  port=$3
+  until docker exec ${container} bash -c "nc ${ip} ${port} < /dev/null"
+  do
+    sleep 1
+  done
+}
+
+wait_for_stop() {
+  container=$1
+  while docker exec ${container} bash -c "ps aux | grep [m]emcached"
+  do
+    sleep 1
+  done
+}
+
+verify_stopped() {
+  container=$1
+  run docker exec ${container} bash -c "ps aux | grep [m]emcached"
+  echo_lines
+  [ "$status" -eq 1 ] 
+}
+
+insert_test_data() {
+  container=$1
+  ip=$2
+  port=$3
+  key=$4
+  data=$5
+  run docker exec ${container} bash -c "printf 'set ${key} 0 60 4\r\n${data}\r\n' | nc ${ip} ${port}"
+}
+
+verify_test_data() {
+  container=$1
+  ip=$2
+  port=$3
+  key=$4
+  data=$5
+  run docker exec ${container} bash -c "printf 'get ${key}\r\n' | nc ${ip} ${port}"
+  data=$(echo -e "${data}\r")
+  echo_lines
+  [ "${lines[1]}" = "${data}" ]
+  [ "$status" -eq 0 ]
+}
+
+verify_plan() {
+  [ "${lines[0]}" = "{" ]
+  [ "${lines[1]}" = "  \"redundant\": false," ]
+  [ "${lines[2]}" = "  \"horizontal\": false," ]
+  [ "${lines[3]}" = "  \"users\": [" ]
+  [ "${lines[4]}" = "  ]," ]
+  [ "${lines[5]}" = "  \"ips\": [" ]
+  [ "${lines[6]}" = "    \"default\"" ]
+  [ "${lines[7]}" = "  ]," ]
+  [ "${lines[8]}" = "  \"port\": 11211," ]
+  [ "${lines[9]}" = "  \"behaviors\": [" ]
+  [ "${lines[10]}" = "    \"migratable\"" ]
+  [ "${lines[11]}" = "  ]" ]
+  [ "${lines[12]}" = "}" ]
+}
